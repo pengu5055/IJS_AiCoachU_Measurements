f828debef758ccc2160021a832f95e7242a2d89e
pengu5055 <urbancmarko1@gmail.com>
2022-07-27 12:33:39 +0200

    First good read

--- Test_Walk/graphs.py
+++ Test_Walk/graphs.py
@@ -3,15 +3,14 @@ import matplotlib.pyplot as plt
 import cmasher as cmr
 import pandas as pd
 
+colors = cmr.take_cmap_colors("cmr.torch", 20, cmap_range=(0.2, 0.8), return_fmt="hex")
+
 # Import tables
-alpha = pd.read_table("alpha.csv", sep=",", header=2).to_numpy()
+# alpha = pd.read_table("alpha.csv", sep=",", header=2, dtype={'AccX [mg]':'int16'}).to_numpy()
+alpha = np.genfromtxt("alpha.csv", delimiter=",", skip_header=2, invalid_raise=False)
 
-# Use boolean mask
-mask = (alpha[:, 0] == "D")
-a_data = alpha[mask, :]
+time = alpha[:, 2]
+acc_x = alpha[:, 3]
 
-
-time = alpha[:, 3].astype("float32")
-print(time)
-plt.plot(time, alpha[:, 4])
+plt.plot(time, acc_x, color=colors[5], lw=2)
 plt.show()
